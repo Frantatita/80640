f5ac35d440d339ea3750d049ef0c3627459e7ac7
Frantatita <reyes.23.jacome@gmail.com>
2023-10-18 14:11:39 -0600

    Practica09

--- Practica08/src/MiFieldSet.jsx
+++ Practica08/src/MiFieldSet.jsx
@@ -1,20 +1,16 @@
-
-
-
-function MiFieldSet(props){
-    const {titulo,txt1,txt2} = props
-        return(
-            <>
-                <fieldset>
-                    <legend>{Titulo}</legend>
-                    <label htmlFor={txt1}>{txt1}</label>
-                    <input type={Text} id="txt1">
-                    <label htmlFor={txt2}>{txt2}</label>
-                    <input type={Text} id="txt2">
-
-                </fieldset>
-            </>  
-        )
-}
-
-export default MiFieldSet
+function MiFieldset(props) {
+    const { titulo, text1, text2 } = props;
+    return (
+      <>
+        <fieldset>
+          <legend>{titulo}</legend>
+          <label htmlFor="text1">{text1}</label>
+          <input type="text" id={text1} />
+          <label htmlFor="text1">{text2}</label>
+          <input type="text" id={text2} />
+        </fieldset>
+      </>
+    );
+  }
+  
+  export default MiFieldset;
--- Practica08/src/main.jsx
+++ Practica08/src/main.jsx
@@ -7,6 +7,8 @@ import MiFieldSet from './MiFieldSet.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<App /> */}
-    <MiFieldSet titulo="Datos personales" txt1="Nombre" txt2="Password"/>
+    <MiFieldSet titulo="Datos personales" text1="Nombre: " text2="Password: "/>
+    <MiFieldSet titulo="Datos personales" text1="Nombre: " text2="Password: "/>
+    <input type="submit" value="Enviar datos" />
   </React.StrictMode>,
 )
