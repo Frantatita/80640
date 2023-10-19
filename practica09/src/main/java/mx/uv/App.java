a01bb9294f9aba323600c2b790f3ae2865a3944c
Frantatita <reyes.23.jacome@gmail.com>
2023-10-19 12:55:32 -0600

    Practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,13 +1,32 @@
-package mx.uv;
-
 /**
  * Hello world!
  *
  */
+
+package mx.uv;
+
+import static spark.Spark.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!" );
+    
+        get("/", (request, response) -> {
+            return "<h1>Hola mundo</h1>";
+        });
+
+        get("/ruta1", (request, response) -> {
+            return "<h1>Adios mundo</h1>";
+        });
+
+        get("/ruta2", (request, response) -> {
+            return "<h1>Mundooooooooo</h1>";
+        });
+
+        get("/ruta3", (request, response) -> {
+            return "<h1>OK Mundo</h1>";
+        });
+       
     }
 }
