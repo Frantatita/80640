df42179efccbd9df2bac81852f8e021bedb3aece
Frantatita <reyes.23.jacome@gmail.com>
2023-11-15 20:57:14 -0600

    Practica 10

--- practica10/src/Formulario.jsx
+++ practica10/src/Formulario.jsx
@@ -1,68 +1,62 @@
+
 import { Box, Button, TextField } from "@mui/material"
-import { useState } from "react";
-import  axios  from "axios";
+import axios from "axios"
+import { useState } from "react"
 
-function Formulario(props) {
+function Formulario(props){
     const [Cargando, setCargando] = useState(false)
-    const [datosFormulario, setDatosFormulario] = useState(
-        {nombre: '', password: ''}
-    )
-
-    const hacerPeticion = async () => {
-        try{
-            const res = await axios.get('http://localhost:4567')
-            console.log(res.data)
+    const [datosFormulario, setDatosFormulario] = useState({
+        nombre: '',
+        password: ''
+    })
+    const hacerPeticion = async () =>{
+        try {
+            const res = await axios.get('http://localhost:4567/ruta4',{params: datosFormulario})
             return res.data
-        }catch (error){
+        } catch (error) {
             throw error
         }
     }
-
-    const procesarFormulario = async (e) => {
+    const procesarFormulario = async (e)=>{
         e.preventDefault()
-        console.log("Datos recuperados del formulario: ", datosFormulario)
+        console.log("datos Recuperados del formulario", datosFormulario)
         setCargando(true)
         try {
             const res = await hacerPeticion()
             console.log(res)
             setCargando(false)
             if(datosFormulario.nombre == res.alumno){
-                console.log('El usuario se encuentra')
+                console.log("El usuario Existe")
             }else{
-                console.log('El usuario no se encuentra')
+                console.log("El usuario NO Existe")
             }
         } catch (error) {
-            console.log('Error', error)
+            console.log(error);
             setCargando(false)
         }
     }
-
-    const cambiosFormulario = (e) =>{
-        //console.log(e.target)
-        const {name, value} = e.target
-        setDatosFormulario ({
-            ...datosFormulario,
-            [name]: value
+    const cambiosFormulario = (e)=>{
+        const {name, value}= e.target
+        setDatosFormulario({
+            ...datosFormulario, [name]:value
         })
     }
-
-  return (
-    <>
-        <form onSubmit={procesarFormulario}> 
+    return(
+        <>
         <h1>Inicio de Sesión</h1>
-            <Box m={5}> 
-                <TextField label="Nombre:" variant="outlined" fullWidth onChange={cambiosFormulario} name="nombre" value={datosFormulario.nombre}></TextField>
+        <form onSubmit={procesarFormulario}>
+            <Box m={5}>
+                <TextField label="Nombre: " variant="outlined" fullWidth onChange={cambiosFormulario} name="nombre" value={datosFormulario.nombre}></TextField>
             </Box>
-            <Box m={5}> 
-                <TextField label="Contraseña:" variant="outlined" type="password" fullWidth onChange={cambiosFormulario} name="password" value={datosFormulario.password}></TextField>
+            <Box m={5}>
+                <TextField label="Contraseña: " variant="outlined" type="password" fullWidth onChange={cambiosFormulario} name="password" value={datosFormulario.password}></TextField>
             </Box>
-            <Box m={5}> 
-                <Button variant="contained" type= "submit" color="primary" fullWidth disabled={Cargando}>Iniciar Sesión</Button>
+            <Box m={5}>
+                <Button variant="contained" type="submit" color="primary" fullWidth disabled={Cargando}>Iniciar Sesión</Button>
             </Box>
-
         </form>
-    </>
-  )  
+        </>
+    )
 }
 
 export default Formulario
--- practica10/src/main.jsx
+++ practica10/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Formulario from './Formulario'
+import Formulario from './Formulario.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
